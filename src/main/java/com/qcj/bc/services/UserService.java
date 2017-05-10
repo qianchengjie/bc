29791d07cbe51d6d4cbcb731dc1668687ff8dc61
qianchengjie <330616153@qq.com>
2017-05-10 19:18:56 +0800

    头像显示修复，以及乱码

--- src/main/java/com/qcj/bc/services/UserService.java
+++ src/main/java/com/qcj/bc/services/UserService.java
@@ -78,7 +78,7 @@ public class UserService {
 		else if(userRepository.getEmail( user.getEmail() ) != null)
 			msg = "邮箱已被注册";
 		else{
-			user.setImgSrc("..");
+			user.setImgSrc("/img/index/person.png");
 			user.setRegDate(new Date());
 			userRepository.save(user);
 		}
