3f95778b2fae8d36db13bb913b03ecc6c0e14099
rian <1051333460@qq.com>
2017-05-14 22:05:41 +0800

    古镇页面++

---
+++ src/main/resouces/static/js/OldStreet.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	var count=0.1;
+	// $("#body #content").find('#items').each(function(index, el) {
+	// 	$(el).css('animation', 'up-in '+count+'s');
+	// 	count+=0.1;
+	// });
+	$("#body #content").find('#items').each(function(index, el) {
+		$(el).css('transform', 'translate(0px,0px)');
+		$(el).css('transition-delay', count+"s");
+		 count+=0.1;
+	});
+});
+$("#body #content #items #img").mouseover(function(event) {
+	/* Act on the event */
+	$(this).find('#mask').css({
+		'transition': '1s',
+		'transform': 'translate(0,0)'
+	});
+});
+$("#body #content #items #img").mouseout(function(event) {
+	/* Act on the event */
+	$(this).find('#mask').css({
+		'transition': '1s',
+		'transform': 'translate(0,-150px)'
+	});
+});
