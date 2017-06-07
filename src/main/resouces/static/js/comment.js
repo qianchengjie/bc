ad6755684208fe12cf660caad7c3772f9dd3f25f
rian <1051333460@qq.com>
2017-06-07 09:24:38 +0800

    最后完善

--- src/main/resouces/static/js/comment.js
+++ src/main/resouces/static/js/comment.js
@@ -3,3 +3,6 @@ $(".comment-bar").click(function(event) {
 	$(".comment-box").slideToggle();
 	$("#tip-box").children('#tip').first().attr('id', 'tip-out');
 });
+$(document).ready(function() {
+	$(".comment-box").slideToggle(0);
+});
